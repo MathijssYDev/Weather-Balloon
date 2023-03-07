fcabac379dec76f7916e39fc891a49dbd77eaffa
Mathijs <MathijssY@gmail.com>
2023-03-07 21:44:55 +0100

    Test

---
+++ Documents/NetBeansProjects/WeatherBalloon/src/main/java/priv/project/weatherballoon/WeatherBalloon.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package priv.project.weatherballoon;
+
+/**
+ *
+ * @author HP
+ */
+public class WeatherBalloon {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
